b9c9524e5aa28fcf06eb152fff7f069fcaf3ef61
Marcos Mellado <marcos@airtame.com>
2017-07-30 22:30:41 +0200

    Fixed meta and removed fxml from fb sdk

--- src/app.js
+++ src/app.js
@@ -7,6 +7,14 @@ import Router from './router';
 
 import './scss/main.scss';
 
+(function(d, s, id){
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
 class App extends Component {
   render() {
     return (
@@ -17,21 +25,14 @@ class App extends Component {
   }
 }
 
-window.fbAsyncInit = function() {
+ window.fbAsyncInit = function() {
   FB.init({
     appId      : '1639130619470630',
-    cookie     : true,
-    xfbml      : true,
-    version    : 'v2.8'
+    xfbml      : false,
+    version    : 'v2.10'
   });
   FB.AppEvents.logPageView();
   render(<App />, document.getElementById('app'));
 };
 
-(function(d, s, id){
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) {return;}
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -168,8 +168,14 @@ class Home extends Component {
               <img src={imgSrc} />
             </div>
             <div className="poster-text">
-              <h1>Opps! We are not available :(</h1>
-              <p>We are currently undergoing some maintenance. We should be back shortly :)</p>
+              <h1>Keep track of the countries you have visited</h1>
+              <p>Just login with Facebook to get started</p>
+              <FacebookLogin
+                  appappId="1639130619470630"
+                  autoLoad={true}
+                  fields="public_profile,email"
+                  callback={this.checkAndSetLoginStatus}
+              />
             </div>
           </div>
         </div>
