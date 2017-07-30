ee98dcbe46692d88246a0a614cb74afd641935ef
Marcos Mellado <marcos@airtame.com>
2017-07-30 16:20:37 +0200

    Fixed stroke-width prop

--- src/components/SearchBox/index.js
+++ src/components/SearchBox/index.js
@@ -101,8 +101,8 @@ class SearchBox extends Component {
               />
               <button className="close" onClick={this.enableSearch}>
                 <svg viewPort="0 0 12 12" version="1.1" xmlns="http://www.w3.org/2000/svg" className="close-icon">
-                    <line x1="1" y1="11" x2="11" y2="1" stroke="black" stroke-width="2"/>
-                    <line x1="1" y1="1" x2="11" y2="11" stroke="black" stroke-width="2"/>
+                    <line x1="1" y1="11" x2="11" y2="1" stroke="black" strokeWidth="2"/>
+                    <line x1="1" y1="1" x2="11" y2="11" stroke="black" strokeWidth="2"/>
                 </svg>
               </button>
               <ul>
