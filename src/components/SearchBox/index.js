0e9d951aa479a881a178a6a292d8967059d28440
Marcos Mellado <marcos@airtame.com>
2017-07-30 23:17:52 +0200

    Updated typeahead close svg

--- src/components/SearchBox/index.js
+++ src/components/SearchBox/index.js
@@ -100,7 +100,7 @@ class SearchBox extends Component {
                 autoFocus
               />
               <button className="close" onClick={this.enableSearch}>
-                <svg viewPort="0 0 12 12" version="1.1" xmlns="http://www.w3.org/2000/svg" className="close-icon">
+                <svg viewBox="0 0 12 12" version="1.1" xmlns="http://www.w3.org/2000/svg" className="close-icon">
                     <line x1="1" y1="11" x2="11" y2="1" stroke="black" strokeWidth="2"/>
                     <line x1="1" y1="1" x2="11" y2="11" stroke="black" strokeWidth="2"/>
                 </svg>
